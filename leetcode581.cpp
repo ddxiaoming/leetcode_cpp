514c3949afe21a067619b675b04a599e4f34bbec
lemon <1750967031@qq.com>
2022-02-09 13:33:28 +0800

    add leetcode581.cpp

---
+++ leetcode581.cpp
@@ -0,0 +1,32 @@
+//
+// Created by lemon on 2022/2/8.
+//
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        vector<int> ori(nums.cbegin(), nums.cend());
+        sort(nums.begin(), nums.end(), less<int>());
+        int sz = static_cast<int>(nums.size()), left = 0, right = sz - 1;
+        while (left <= right && nums[left] == ori[left]) ++left;
+        while (left <= right && nums[right] == ori[right]) --right;
+        return right - left + 1;
+    }
+};
+int main() {
+    Solution solution;
+    vector<int> nums{2, 6, 4, 8, 10, 9, 15};
+    cout << solution.findUnsortedSubarray(nums) << endl;
+
+    nums = {1, 2, 3, 4};
+    cout << solution.findUnsortedSubarray(nums) << endl;
+
+    nums = {1};
+    cout << solution.findUnsortedSubarray(nums) << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by lemon on 2022/2/8.
+//
+int main() {
+    return 0;
+}
