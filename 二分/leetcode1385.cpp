2ac1e9d4ac646e55093df21d0e7265d37d2a396a
lemon <1750967031@qq.com>
2022-05-07 14:14:24 +0800

    add leetcode1385.cpp

--- 二分/leetcode1385.cpp
+++ 二分/leetcode1385.cpp
@@ -5,12 +5,18 @@ using namespace std;
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        sort(arr2.begin(), arr2.end());
+        int arr2_sz = static_cast<int>(arr2.size()) - 1;
         int res = 0;
-        sort(arr2.begin(), arr2.end(), [](const int x, const int y) -> bool {
-            return abs(x) < abs(y);
-        });
-        for (const int arr: arr1) {
-            if (abs(arr - arr2[0]) > d) ++res;
+        for (const int arr : arr1) {
+            auto greater_equal = lower_bound(arr2.cbegin(), arr2.cend(), arr);
+            if (greater_equal == arr2.end()) {
+                res += (arr - arr2[arr2_sz]) > d;
+            } else {
+                bool ok = (arr2[greater_equal - arr2.cbegin()] - arr) > d;
+                if (greater_equal - arr2.cbegin() - 1 >= 0) ok &= (arr - arr2[greater_equal - arr2.cbegin() - 1]) > d;
+                res += ok;
+            }
         }
         return res;
     }
